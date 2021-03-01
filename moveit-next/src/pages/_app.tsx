be9c336284e892e105658328c65005b386257c3e
Patrick Almeida Silva <palmeids@everis.com>
2021-03-01 10:07:42 -0300

    initial context

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import '../styles/global.css';
 
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [level, setLevel] = useState(1);
+
+  function levelUp() {
+
+  }
+
+  return (
+    <ChallengesContext.Provider value={{level: 1, levelUp}}>
+      <Component {...pageProps} />
+    </ChallengesContext.Provider>
+  ) 
 }
 
 export default MyApp
