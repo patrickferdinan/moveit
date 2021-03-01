6e29138c527fd7eba8478826e66bbcae59dfb71b
Patrick Almeida Silva <palmeids@everis.com>
2021-03-01 17:22:31 -0300

    creating challenge context

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,3 +1,37 @@
-import { createContext } from 'react';
+import { createContext, useState, ReactNode } from 'react';
 
-export const ChallengesContext = createContext({});
+
+
+interface ChallengesContextData {
+  level: number;
+  currenExperience: number;
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({children}: ChallengesProviderProps) {
+  const [level, setLevel] = useState(1);
+  const [currenExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  function levelUp() {
+    setLevel(level + 1)
+  }
+
+  function startNewChallenge() {
+    console.log('new');
+  }
+
+  return (
+    <ChallengesContext.Provider value={{level, currenExperience, challengesCompleted, levelUp, startNewChallenge}}>
+      {children}
+    </ChallengesContext.Provider>
+  )
+}
--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,20 +1,16 @@
 import { useState } from 'react';
 import '../styles/global.css';
 
-import { ChallengesContext } from '../contexts/ChallengesContext'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
 
 function MyApp({ Component, pageProps }) {
-  const [level, setLevel] = useState(1);
-
-  function levelUp() {
-
-  }
+  
 
   return (
-    <ChallengesContext.Provider value={{level: 1, levelUp}}>
+    <ChallengesProvider>
       <Component {...pageProps} />
-    </ChallengesContext.Provider>
+    </ChallengesProvider>
   ) 
 }
 
