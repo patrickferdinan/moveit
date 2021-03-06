67f22be3b4674c1dbdea27ad7ac6095fbb0f5a37
Patrick Almeida Silva <palmeids@everis.com>
2021-03-06 16:45:48 -0300

    create modal

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -11,37 +11,42 @@ import { Profile } from "../components/Profile";
 
 import styles from "../styles/pages/Home.module.css";
 import { CountdownProvider } from "../contexts/CountdownContext";
-
-
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
 export default function Home(props) {
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | move.it</title>
-      </Head>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | move.it</title>
+        </Head>
 
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompleteChallenges />
-            <Countdown />     
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompleteChallenges />
+              <Countdown />     
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
-  const user ={
+  const user = {
     level: 1, 
     currentExperience: 50,
     challengesCompleted: 2,
@@ -51,9 +56,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   return {
     props:{
-      level,
-      currentExperience, 
-      challengesCompleted
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
     }
   }
 }
